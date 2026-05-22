2563cf0c17a27b1368b06d619a93c35c98e1b19a
Tasif <tasifhossan01@gmail.com>
2026-05-22 22:17:33 +0600

    Global error handler

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,12 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const globalErrorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ success: false, message });
+};
