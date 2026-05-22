713a00b18ed2c96cd6682529208ffdb928f1a949
Tasif <tasifhossan01@gmail.com>
2026-05-22 23:51:42 +0600

    First version of Application

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./modules/auth/auth.routes";
+import issueRoutes from "./modules/issues/issues.routes";
+import { globalErrorHandler } from "./middleware/error.middleware";
 
+dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_req, res) => res.json({ message: "DevPulse API running" }));
+app.use("/api/auth", authRoutes);
+app.use("/api/issues", issueRoutes);
+
+app.use(globalErrorHandler);
 
 export default app;
