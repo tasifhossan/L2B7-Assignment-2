e2b27105f5186b5a1a9d9217ac402a7b7cd83ae3
Mohammad Tasif Hossan <153765460+tasifhossan@users.noreply.github.com>
2026-05-23 14:03:15 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
